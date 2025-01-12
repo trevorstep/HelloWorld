07ef740353aa1c67f23870052736dc99bbdce18c
Trevor Stephenson <trevors19themail@gmail.com>
2025-01-11 19:30:04 -0700

    main program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿static string Reverse(string text)
+{
+string rev_text = "";
+foreach (char c  in  text)
+{
+rev_text = c + rev_text;
+}
+return rev_text;
+}
+
+
+
+Console.WriteLine("Hello World!!!");
+
+Console.WriteLine(Reverse("Hello World!!!"));
